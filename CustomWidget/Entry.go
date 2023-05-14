eae0684cd2effe0c8452d05d63050ee784be0357
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-05-14 23:04:01 +0200

    [FEATURE] Add support for keyboard shortcuts to custom entry widget

--- CustomWidget/Entry.go
+++ CustomWidget/Entry.go
@@ -2,12 +2,20 @@ package CustomWidget
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
+	"runtime"
+	"strings"
 	"sync"
 )
 
 var _ fyne.Widget = (*EntryWithPopupMenu)(nil)
 var _ fyne.Tabbable = (*EntryWithPopupMenu)(nil)
+
+type EntryWithPopupMenuShortcuts struct {
+	Name    string
+	Handler func()
+}
 
 type EntryWithPopupMenu struct {
 	widget.Entry
@@ -16,8 +24,9 @@ type EntryWithPopupMenu struct {
 	impl         fyne.Widget
 	propertyLock sync.RWMutex
 
-	popUp               *widget.PopUpMenu
-	additionalMenuItems []*fyne.MenuItem
+	popUp                   *widget.PopUpMenu
+	additionalMenuItems     []*fyne.MenuItem
+	shortcutSubmitFunctions []EntryWithPopupMenuShortcuts
 }
 
 func (e *EntryWithPopupMenu) superWidget() fyne.Widget {
@@ -85,4 +94,83 @@ func (e *EntryWithPopupMenu) TappedSecondary(pe *fyne.PointEvent) {
 	}
 	e.popUp = widget.NewPopUpMenu(menu, c)
 	e.popUp.ShowAtPosition(popUpPos)
+}
+
+type ShortcutEntrySubmit struct {
+	fyne.KeyName
+	Modifier fyne.KeyModifier
+	Handler  func()
+}
+
+var _ fyne.Shortcut = (*ShortcutEntrySubmit)(nil)
+var _ fyne.KeyboardShortcut = (*ShortcutEntrySubmit)(nil)
+
+func modifierToString(mods fyne.KeyModifier) string {
+	s := []string{}
+	if (mods & fyne.KeyModifierShift) != 0 {
+		s = append(s, string("Shift"))
+	}
+	if (mods & fyne.KeyModifierControl) != 0 {
+		s = append(s, string("Control"))
+	}
+	if (mods & fyne.KeyModifierAlt) != 0 {
+		s = append(s, string("Alt"))
+	}
+	if (mods & fyne.KeyModifierSuper) != 0 {
+		if runtime.GOOS == "darwin" {
+			s = append(s, string("Command"))
+		} else {
+			s = append(s, string("Super"))
+		}
+	}
+	return strings.Join(s, "+")
+}
+
+func (s ShortcutEntrySubmit) ShortcutName() string {
+	id := &strings.Builder{}
+	id.WriteString("CustomDesktop:")
+	id.WriteString(modifierToString(s.Modifier))
+	id.WriteString("+")
+	id.WriteString(string(s.KeyName))
+	return id.String()
+}
+
+func (s ShortcutEntrySubmit) Key() fyne.KeyName {
+	return s.KeyName
+}
+
+func (s ShortcutEntrySubmit) Mod() fyne.KeyModifier {
+	return s.Modifier
+}
+
+func (e *EntryWithPopupMenu) AddCustomShortcut(shortcutEntrySubmit ShortcutEntrySubmit) {
+	addToSlice := true
+	for _, shortcut := range e.shortcutSubmitFunctions {
+		if shortcut.Name == shortcutEntrySubmit.ShortcutName() {
+			addToSlice = false
+			break
+		}
+	}
+	if addToSlice {
+		e.shortcutSubmitFunctions = append(e.shortcutSubmitFunctions, EntryWithPopupMenuShortcuts{Name: shortcutEntrySubmit.ShortcutName(), Handler: shortcutEntrySubmit.Handler})
+		e.TypedShortcut(shortcutEntrySubmit)
+	}
+}
+
+func (e *EntryWithPopupMenu) TypedShortcut(s fyne.Shortcut) {
+	if _, ok := s.(*desktop.CustomShortcut); !ok {
+		e.Entry.TypedShortcut(s)
+		return
+	}
+
+	// if the shortcut is a submit shortcut, call the submit function
+	if _, ok := s.(*ShortcutEntrySubmit); !ok {
+		for _, shortcut := range e.shortcutSubmitFunctions {
+			if shortcut.Name == s.ShortcutName() && shortcut.Handler != nil {
+				println("shortcut triggered:", s.ShortcutName())
+				shortcut.Handler()
+				return
+			}
+		}
+	}
 }
