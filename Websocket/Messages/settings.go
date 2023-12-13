5576b648ba5ef31574be219b4c9d953f49572b24
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-12-13 16:54:17 +0100

    [TASK] Improve source language combo widget initialization
    [TASK] remove logging

--- Websocket/Messages/settings.go
+++ Websocket/Messages/settings.go
@@ -5,7 +5,6 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
-	"log"
 	"strings"
 	"whispering-tiger-ui/Fields"
 	"whispering-tiger-ui/Settings"
@@ -29,10 +28,6 @@ func (res TranslateSetting) Update() *TranslateSetting {
 
 	Settings.Form = Settings.BuildSettingsForm(nil, Settings.Config.SettingsFilename).(*widget.Form)
 	Settings.Form.Refresh()
-
-	log.Println("InstalledLanguages.GetNameByCode()")
-	log.Println(InstalledLanguages.GetNameByCode(res.Trg_lang))
-	log.Println(res.Trg_lang)
 
 	// fill combo-box with whisper languages
 	if len(Fields.Field.TranscriptionSpeakerLanguageCombo.Options) < len(TranslateSettings.WhisperLanguages) {
@@ -69,12 +64,16 @@ func (res TranslateSetting) Update() *TranslateSetting {
 
 	// Set SourceLanguageCombo
 	if strings.ToLower(Fields.Field.SourceLanguageCombo.Text) != strings.ToLower(InstalledLanguages.GetNameByCode(res.Src_lang)) {
-		Fields.Field.SourceLanguageCombo.Text = cases.Title(language.English, cases.Compact).String(InstalledLanguages.GetNameByCode(res.Src_lang))
+		if Fields.Field.SourceLanguageCombo.Text == "" {
+			Fields.Field.SourceLanguageCombo.Text = cases.Title(language.English, cases.Compact).String(InstalledLanguages.GetNameByCode(res.Src_lang))
+		}
 		if Fields.Field.SourceLanguageTxtTranslateCombo.Text == "" {
 			Fields.Field.SourceLanguageTxtTranslateCombo.Text = cases.Title(language.English, cases.Compact).String(InstalledLanguages.GetNameByCode(res.Src_lang))
 		}
 	} else if Fields.Field.SourceLanguageCombo.Text == "" && strings.ToLower(res.Src_lang) == "auto" {
-		Fields.Field.SourceLanguageCombo.Text = cases.Title(language.English, cases.Compact).String(res.Src_lang)
+		if Fields.Field.SourceLanguageCombo.Text == "" {
+			Fields.Field.SourceLanguageCombo.Text = cases.Title(language.English, cases.Compact).String(res.Src_lang)
+		}
 		if Fields.Field.SourceLanguageTxtTranslateCombo.Text == "" {
 			Fields.Field.SourceLanguageTxtTranslateCombo.Text = cases.Title(language.English, cases.Compact).String(res.Src_lang)
 		}
@@ -110,6 +109,7 @@ func (res TranslateSetting) Update() *TranslateSetting {
 	if checkValue != res.Tts_answer {
 		Fields.DataBindings.TextToSpeechEnabledDataBinding.Set(res.Tts_answer)
 	}
+
 	checkValue, _ = Fields.DataBindings.OSCEnabledDataBinding.Get()
 	if checkValue != res.OscAutoProcessingEnabled {
 		Fields.DataBindings.OSCEnabledDataBinding.Set(res.OscAutoProcessingEnabled)
